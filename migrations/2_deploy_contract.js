b9677549a5840f8cd64cf53dc150136e37ff2ed3
yehoudaB <bouskilayehouda@gmail.com>
2022-01-25 22:21:07 +0100

    fix migrations/2deploy_contract.js

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,7 @@
-const { web3 } = require("@openzeppelin/test-helpers/src/setup");
 const Wallet = artifacts.require("Wallet");
 
-module.exports = async function (deployer, _network, accounts) {
-  await deployer.deploy(Wallet, [accounts[0], accounts[1],accounts[2]],2);
-  const wallet =  await Wallet.deployed();
-  await web3.eth.sendTransaction({from:accounts[0], to: wallet.address, value: 10000});
+module.exports = async function(deployer, _network, accounts) {
+  await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
+  const wallet = await Wallet.deployed();
+  await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
 };
